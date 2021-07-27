3b293a3a6453bf3b93ae7854bfb6d142ec92672a
unknown <dongdev@fasoo.com>
2021-07-27 11:26:37 +0900

    클릭한 위치에 div요소 추가

--- test/js/content_script.js
+++ test/js/content_script.js
@@ -1,13 +1,35 @@
 // // content_script.js
 function onWindowLoad() {
-  console.log("팝업 페이지의 DOM 접근 : ");
+  console.log("팝업 페이지의 DOM 접근 : ");  
   // 위 결과는 위 html 파일에서 product_name 아이디의 태그 값인 Injecting Product Name.... 이 된다.  
 }
 
 window.onload = onWindowLoad;
 
-window.addEventListener('mouseup',()=>{
-  alert(selectText())
+// const modal = document.getElementById("modal")
+// var newDIV = document.createElement("div");​
+// newDIV.innerHTML = "새로 생성된 DIV입니다.";
+let div = document.createElement('div')  
+let txt = document.createTextNode('');
+div.style.width= "100px"
+div.style.height= "100px"
+
+
+window.addEventListener('mouseup',(event)=>{
+  // modal.style.display="flex"
+  div.innerHTML=""
+  let x = event.clientX + 10
+  let y = event.clientY - 100      
+  div.style.left = `${x}px`;
+  div.style.top = `${y}px`; 
+  div.style.position = "absolute"
+  div.style.float = "true"  
+  div.style.backgroundColor="tomato"
+  txt = document.createTextNode(selectText());
+  div.appendChild(txt)    
+  document.body.appendChild(div)
+  
+  // alert(selectText())
 })
 
 function selectText() {
--- test/script.js
+++ test/script.js
@@ -2,8 +2,8 @@
 // alert(bodyText);
 
 //크롬 확장의 기능 중에 tabs과 관련된 기능 중에 컨텐츠 페이지를 대상으로 아래와 같은 코드를 실행한다
-chrome.tabs.executeScript({
-  code:'document.querySelector("body").innerText'
-}, function(result) {
-  alert(result[0])
-});
+// chrome.tabs.executeScript({
+//   code:'document.querySelector("body").innerText'
+// }, function(result) {
+//   alert(result[0])
+// });
