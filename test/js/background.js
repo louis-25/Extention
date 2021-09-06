59beecf78a0f4a0a9b9f437594d972b24719629d
louis-25 <20151577@vision.hoseo.edu>
2021-09-06 09:16:53 +0900

    폴더삭제

--- test/js/background.js
+++
@@ -1,9 +0,0 @@
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    window.addEventListener('dblclick', (e)=>{
-        console.log('background ', e)
-    })
-    console.log('backend ',request)
-    
-    if (request.action === "FINISH")
-        sendResponse({farewell: "goodbye"});
-});
